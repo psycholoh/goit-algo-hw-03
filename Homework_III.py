cd84eb7e4a0d1f493b7b7e71836ff3a9c3918209
psycholoh <88897957+psycholoh@users.noreply.github.com>
2025-06-11 12:09:51 +0300

    Update Homework_III.py

--- Homework_III.py
+++ Homework_III.py
@@ -19,7 +19,7 @@ if days_difference is not None:
 
 import random
 def get_numbers_ticket(min, max, count):
-    if min > max or min < 1 or (max-min)<count:
+    if min > max or min < 1 or (max-min+1)<count:
         return[]
     numbers = random.sample(range(min, max + 1), count)
     return sorted(numbers)
